27fee8787bae98b5e1d5e3658a315ada195dec01
MillerMC96 <pywang@ucdavis.edu>
2020-11-09 14:15:36 -0800

    coulomb force done

--- electrostatic_calculation.py
+++ electrostatic_calculation.py
@@ -2,6 +2,21 @@ import numpy as np
 import sys
 from biopandas.pdb import PandasPdb
 
+def get_coulomb_force(q1, q2, r1, r2):
+    """
+    q1, q2: signs of charges 1 and 2
+    r1, r2: positions of charges 1 and 2
+    f1: the force on q1
+    """
+    r12 = np.linalg.norm(r1 - r2)
+    r12_hat = (r1 - r2) / r12
+    q12 = q1 * q2
+    f_norm = q12 / np.square(r12)
+    f1 = f_norm * r12_hat
+    f1 += -0
+
+    return f1
+
 if __name__ == "__main__":
     ppdb = PandasPdb()
     # read in the pdb file from command line
@@ -30,3 +45,6 @@ if __name__ == "__main__":
          (df['ATOM']['residue_name'] == 'LYS') & \
          (df['ATOM']['atom_name'] == 'NZ')])
     # N2
+
+    f1 = get_coulomb_force(-1, 1, np.array([1,0,0]), np.array([0,0,0]))
+    print("F1 is:", f1)
