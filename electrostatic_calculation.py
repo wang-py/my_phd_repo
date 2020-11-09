ec20260a3ad25208d84d2c2644fadf17e07d9817
MillerMC96 <pywang@ucdavis.edu>
2020-11-09 11:47:01 -0800

    find chain 6

--- electrostatic_calculation.py
+++ electrostatic_calculation.py
@@ -1,2 +1,10 @@
 import numpy as np
+import sys
 from biopandas.pdb import PandasPdb
+
+if __name__ == "__main__":
+    ppdb = PandasPdb()
+    # read in the pdb file from command line
+    ppdb.read_pdb(sys.argv[1])
+    df = ppdb.df
+    print(df['ATOM'][df['ATOM']['chain_id'] == '6'])
