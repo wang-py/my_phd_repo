1cdfea1f96c7b591d69b47e6b8e6e7f2f6c7aeb2
MillerMC96 <pywang@ucdavis.edu>
2020-11-27 14:15:09 -0800

    switched to pymol api

--- get_chainH.py
+++ get_chainH.py
@@ -1,6 +1,6 @@
 import numpy as np
 import sys
-from biopandas.pdb import PandasPdb
+from pymol import cmd
 
 ###############################################################################
 # this script strips chain H from the input structure                         #
@@ -8,11 +8,8 @@ from biopandas.pdb import PandasPdb
 # Email: pywang@ucdavis.edu                                                   #
 ###############################################################################
 if __name__ == "__main__":
-    ppdb = PandasPdb()
     # read in the pdb file from command line
-    ppdb.read_pdb(sys.argv[1])
+    cmd.load(sys.argv[1])
     # output path
     output_path = sys.argv[1].split('.')[0] + "_chainH.pdb"
-    df = ppdb.df
-    df = df['ATOM'][df['ATOM']['chain_id'] == 'H']
-    ppdb.to_pdb(output_path)
+    cmd.save(output_path, selection=chainH)
