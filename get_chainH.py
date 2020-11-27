db5070a55026ef0f7fb05dd367f355b1209a880c
MillerMC96 <pywang@ucdavis.edu>
2020-11-27 13:56:13 -0800

    get chain H script

---
+++ get_chainH.py
@@ -0,0 +1,9 @@
+import numpy as np
+import sys
+from biopandas.pdb import PandasPdb
+
+###############################################################################
+# this script strips chain H from the input structure                         #
+# Author: Panyue Wang                                                         #
+# Email: pywang@ucdavis.edu                                                   #
+###############################################################################
