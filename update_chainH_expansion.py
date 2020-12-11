e9d56d9f94e1c021e5f71d381ff0440a4834409c
MillerMC96 <pywang@ucdavis.edu>
2020-12-11 13:34:54 -0800

    testing one frame reading

--- update_chainH_expansion.py
+++ update_chainH_expansion.py
@@ -2,4 +2,11 @@ import numpy as np
 import sys
 
 if __name__ == "__main__":
+    # mode file
+    mode = open(sys.argv[1], 'r')
+    # lines of one frame
+    frame_lines = 815
+    i = 0
+    one_frame = mode.readlines(frame_lines * i)
+    print(one_frame)
     pass
