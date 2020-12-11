9af395800a2ff11a204a181782ee7050da74ad30
MillerMC96 <pywang@ucdavis.edu>
2020-12-11 15:13:22 -0800

    swapping works

--- update_chainH_expansion.py
+++ update_chainH_expansion.py
@@ -15,12 +15,14 @@ if __name__ == "__main__":
     # from 37 to 75
     frame_index = int(sys.argv[3])
     # output pdb (next step's input)
-    #output_pdb = open(sys.argv[4], 'w')
+    output_pdb = open(sys.argv[4], 'w')
     mode_start = frame_index * frame_lines
     #end = start + frame_lines
     for i in range(frame_lines - 3):
-        input_line = input_lines[chainH_start + i]
-        print(input_line, end="")
-        #mode_line = lines[mode_start + i + 1]
-        #print(mode_line, end="")
+        input_lines[chainH_start + i] = lines[mode_start + i + 1]
+    
+    # swapping the output chain H with the mode chain H
+    for i in range(len(input_lines)):
+        output_pdb.write(input_lines[i])
+
     pass
