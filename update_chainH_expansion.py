8f47c6f69b62de4bbf401e9d2c07bccd5c441565
MillerMC96 <pywang@ucdavis.edu>
2020-12-11 14:51:32 -0800

    input pdb reading

--- update_chainH_expansion.py
+++ update_chainH_expansion.py
@@ -5,14 +5,20 @@ if __name__ == "__main__":
     # mode file
     mode = open(sys.argv[1], 'r')
     lines = mode.readlines()
-    # input pdb
+    # input pdb (last step's output)
     input_pdb = open(sys.argv[2], 'r')
+    input_lines = input_pdb.readlines()
+    # starting line of chain H
+    chainH_start = 620
     # lines of one frame
     frame_lines = 814
+    for i in range(chainH_start, chainH_start+frame_lines):
+        input_line = input_lines[i]
+        print(input_line, end="")
     # from 37 to 75
     frame_index = int(sys.argv[3])
-    # output pdb
-    output_pdb = open(sys.argv[4], 'w')
+    # output pdb (next step's input)
+    #output_pdb = open(sys.argv[4], 'w')
     start = frame_index * frame_lines
     end = start + frame_lines
     for i in range(start, end):
