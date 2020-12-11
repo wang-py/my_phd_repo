886080509a429f1b7662f437c9a3e53ecef4b794
MillerMC96 <pywang@ucdavis.edu>
2020-12-11 13:48:04 -0800

    indexing successful

--- update_chainH_expansion.py
+++ update_chainH_expansion.py
@@ -4,9 +4,13 @@ import sys
 if __name__ == "__main__":
     # mode file
     mode = open(sys.argv[1], 'r')
+    lines = mode.readlines()
     # lines of one frame
     frame_lines = 814
-    for i in range(frame_lines):
-        one_line = mode.readline()
-        print(one_line, end="")
+    frame_index = 38
+    start = frame_index * frame_lines
+    end = start + frame_lines
+    for i in range(start, end):
+        line = lines[i]
+        print(line, end="")
     pass
