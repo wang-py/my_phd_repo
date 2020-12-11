9d97209683b968ed2b195cfdab3e822ee196b379
MillerMC96 <pywang@ucdavis.edu>
2020-12-11 14:21:34 -0800

    started swapping

--- update_chainH_expansion.py
+++ update_chainH_expansion.py
@@ -5,9 +5,14 @@ if __name__ == "__main__":
     # mode file
     mode = open(sys.argv[1], 'r')
     lines = mode.readlines()
+    # input pdb
+    input_pdb = open(sys.argv[2], 'r')
     # lines of one frame
     frame_lines = 814
-    frame_index = 38
+    # from 37 to 75
+    frame_index = int(sys.argv[3])
+    # output pdb
+    output_pdb = open(sys.argv[4], 'w')
     start = frame_index * frame_lines
     end = start + frame_lines
     for i in range(start, end):
