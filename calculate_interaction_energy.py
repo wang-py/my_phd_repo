6feb4509202ad8a73335ce180df14e372586f063
MillerMC96 <pywang@ucdavis.edu>
2021-02-04 15:19:33 -0800

    added coulomb energy

--- calculate_interaction_energy.py
+++ calculate_interaction_energy.py
@@ -3,6 +3,27 @@ import matplotlib.pyplot as plt
 import scipy.constants as sc
 import sys
 
+def get_coulomb_potential(qij, R):
+    """
+    calculate the coulomb potential between qi and qj
+    ---------------------------
+    qij:
+    product of charges on i and j in e^2
+
+    R:
+    distance between two charges in angstrom
+
+    Returns
+    ---------------------------
+    Uc: float
+    Coulomb potential energy of the system in kJ/mol
+    """
+    #Coulomb constant in kJ/mol*A/e^2
+    k0 = 1389.35456 
+    Uc = k0 * qij / R
+
+    return Uc
+
 if __name__ == "__main__":
     input_pdb = open(sys.argv[1], 'r')
     atom_index = int(sys.argv[2])
