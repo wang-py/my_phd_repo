b21a4e1bd687182e6f92aae42b80c64688a4ffed
MillerMC96 <pywang@ucdavis.edu>
2021-02-04 15:13:54 -0800

    init

---
+++ calculate_interaction_energy.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.constants as sc
+import sys
+
+if __name__ == "__main__":
+    input_pdb = open(sys.argv[1], 'r')
+    atom_index = int(sys.argv[2])
+    exit()
