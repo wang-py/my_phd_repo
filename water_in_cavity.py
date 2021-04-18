0e54228f605cd5edcd526a85384b87d6c521d61d
MillerMC96 <pywang@ucdavis.edu>
2021-04-18 15:01:24 -0700

    todos

--- water_in_cavity.py
+++ water_in_cavity.py
@@ -2,3 +2,7 @@ import numpy as np
 from biopandas.pdb import PandasPdb
 import matplotlib.pyplot as plt
 
+# script to find out water molecules (oxygens) that are inside of a cavity
+# TODO: cavity preprocessing: split cavity in half by z coordinates, exclude
+# waters that are out side the two boxes.
+# TODO: find enclosed water molecules
