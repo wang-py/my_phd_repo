6005f83dc14a471d189632380f09137c97824c26
MillerMC96 <pywang@ucdavis.edu>
2021-09-28 14:12:19 -0700

    init

---
+++ protein_dimensionality_analysis.py
@@ -0,0 +1,11 @@
+# Script used to analyze the dimensionality of protein structure
+# Author: Panyue Wang
+# Email: pywang@ucdavis.edu
+
+import numpy as np
+import matplotlib.pyplot as plt
+from biopandas.pdb import PandasPdb
+
+# TODO: function that reads in the pdb structure
+# TODO: function for selecting atoms based on radius around a certain atom
+# TODO: plotting function that plots log # of atoms vs log(radius)
