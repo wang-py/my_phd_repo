5071b9ea590f2a289f309d04c30fe876c8022823
MillerMC96 <pywang@ucdavis.edu>
2021-09-28 14:59:58 -0700

    version 0.8

--- protein_dimensionality_analysis.py
+++ protein_dimensionality_analysis.py
@@ -2,16 +2,61 @@
 # Author: Panyue Wang
 # Email: pywang@ucdavis.edu
 
+from os import read
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
 from biopandas.pdb import PandasPdb
-from calculate_interaction_energy import apply_cutoff, parse_pdb
+from calculate_interaction_energy import get_distance_vec, parse_pdb, assign_params, read_topology
 
 # TODO: plotting function that plots log # of atoms vs log(radius)
+
+# distance cutoff function
+def apply_cutoff(r_cutoff, atom_index, coords_params):
+    """
+    apply distance cutoff for coulomb interactions
+    ----------------------------------------------
+    r_cutoff: float
+    cutoff distance in angstroms
+
+    atom_index: int
+    index of focus atom
+
+    coords_params: ndarray
+    parameters of atoms
+
+    Returns: 
+    ----------------------------------------------
+    trunc_params: ndarray
+    truncated vector of distances within cutoff range
+    """
+    atom_dist = get_distance_vec(atom_index, coords_params[:,0:3])
+    coords_params_dist = np.c_[coords_params, atom_dist]
+    atoms_within_cutoff = np.array([x[-1] < r_cutoff for x in coords_params_dist])
+    # include self
+    #atoms_within_cutoff[atom_index] = True
+    atoms_in_r = coords_params[atoms_within_cutoff]
+
+    return atoms_in_r
 
 if __name__ == "__main__":
     input_pdb = sys.argv[1]
     selected_atom = int(sys.argv[2])
     ppdb = PandasPdb()
+    # convert pdb into a data frame
     input_df = parse_pdb(ppdb, input_pdb)
+    # read in topology
+    topology = read_topology(open("topology.dat"))
+    # building data structure with properties of the atoms
+    coords_params = assign_params(input_df, topology)
+    # array of radii
+    radius_range = np.linspace(0,10,21)
+    # applying cutoff based on distance
+    atoms_within_cutoff_arr = []
+    number_of_atoms_arr = []
+    for radius in radius_range:
+        atoms_within_cutoff = apply_cutoff(radius, selected_atom, coords_params)
+        atoms_within_cutoff_arr.append(atoms_within_cutoff)
+        number_of_atoms = len(atoms_within_cutoff)
+        number_of_atoms_arr.append(number_of_atoms)
+        print("Number of atoms within %f angstroms is %d"%(radius, number_of_atoms))
