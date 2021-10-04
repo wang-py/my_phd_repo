8359e507c299bc157f26a761d3e7552ac6a88f2e
MillerMC96 <pywang@ucdavis.edu>
2021-10-04 16:17:38 -0700

    added a function that selects a tunnel

--- caver_plot.py
+++ caver_plot.py
@@ -10,15 +10,19 @@ def plotting(x, y, label):
     plt.ylabel("Radius [A]")
     plt.title("sphere size along caver points")
 
+def select_tunnel_to_plot(tunnel_index, input_csv):
+    profile_df = pd.read_csv(input_csv, skiprows= 7 * tunnel_index - 2, nrows=1)
+    R_range = profile_df.iloc[0, 13:]
+
+    return R_range
 
 if __name__ == "__main__":
     # input csv file
     input_csv = sys.argv[1]
     # reading csv into a pandas dataframe
-    profile_df = pd.read_csv(input_csv, skiprows=26, nrows=1)
-    R_range = profile_df.iloc[0, 13:]
-    pts_range = range(len(R_range))
-    plotting(pts_range, R_range, "Q10")
+    R_range_Q =  select_tunnel_to_plot(4, input_csv)
+    pts_range = range(len(R_range_Q))
+    plotting(pts_range, R_range_Q, "Q10")
     plt.legend()
     plt.show()
     pass
