5f9844be55e6752291373fd3be015034eccf6d4e
MillerMC96 <pywang@ucdavis.edu>
2021-10-04 14:42:20 -0700

    reading csv

--- caver_plot.py
+++ caver_plot.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
 # Script that reads the csv output of caver and plots tunnel profiles
 
 if __name__ == "__main__":
+    # input csv file
+    input_csv = sys.argv[1]
+    # reading csv into a pandas dataframe
+    profile_df = pd.read_csv(input_csv)
     pass
