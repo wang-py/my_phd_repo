00ce61540300ce71ebb91db1885a732fc9d00d99
MillerMC96 <pywang@ucdavis.edu>
2021-10-04 15:37:55 -0700

    plotting

--- caver_plot.py
+++ caver_plot.py
@@ -7,5 +7,13 @@ if __name__ == "__main__":
     # input csv file
     input_csv = sys.argv[1]
     # reading csv into a pandas dataframe
-    profile_df = pd.read_csv(input_csv)
+    profile_df = pd.read_csv(input_csv, skiprows=26, nrows=1)
+    R_range = profile_df.iloc[0, 13:]
+    pts_range = range(len(R_range))
+    plt.figure()
+    plt.scatter(pts_range, R_range)
+    plt.xlabel("points")
+    plt.ylabel("Radius [A]")
+    plt.title("Q10 size along caver points")
+    plt.show()
     pass
