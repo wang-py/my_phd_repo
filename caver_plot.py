47f0c20d17fb0f6a9f196056d21e20572a961baa
MillerMC96 <pywang@ucdavis.edu>
2021-10-04 16:07:53 -0700

    refactor plotting into a function

--- caver_plot.py
+++ caver_plot.py
@@ -3,6 +3,14 @@ import matplotlib.pyplot as plt
 import sys
 # Script that reads the csv output of caver and plots tunnel profiles
 
+def plotting(x, y, label):
+    plt.figure()
+    plt.scatter(x, y, label=label)
+    plt.xlabel("points")
+    plt.ylabel("Radius [A]")
+    plt.title("sphere size along caver points")
+
+
 if __name__ == "__main__":
     # input csv file
     input_csv = sys.argv[1]
@@ -10,10 +18,7 @@ if __name__ == "__main__":
     profile_df = pd.read_csv(input_csv, skiprows=26, nrows=1)
     R_range = profile_df.iloc[0, 13:]
     pts_range = range(len(R_range))
-    plt.figure()
-    plt.scatter(pts_range, R_range)
-    plt.xlabel("points")
-    plt.ylabel("Radius [A]")
-    plt.title("Q10 size along caver points")
+    plotting(pts_range, R_range, "Q10")
+    plt.legend()
     plt.show()
     pass
