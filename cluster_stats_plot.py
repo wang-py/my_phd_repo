e45375d54a8b6d0b3c2d5038dde662b0f98d9ffb
MillerMC96 <pywang@ucdavis.edu>
2021-10-18 11:34:47 -0700

    cluster plot init

---
+++ cluster_stats_plot.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+
+# Script that reads the csv output of glances and nvidia-smi and plots the 
+# results 
+
+def read_cpu_csv(cpu_csv):
+    pass
+
+def read_gpu_csv(gpu_csv):
+    pass
+
+def cpu_plot(time, temp, usage):
+    pass
+
+def gpu_plot(time, temp, usage):
+    pass
+
+if __name__ == "__main__":
+    pass
