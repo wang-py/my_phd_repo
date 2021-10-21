1b30cb597f5038708aab2859df3930fe44381dc7
MillerMC96 <pywang@ucdavis.edu>
2021-10-20 18:02:19 -0700

    plotting function

--- cluster_stats_plot.py
+++ cluster_stats_plot.py
@@ -6,16 +6,37 @@ import sys
 # results 
 
 def read_cpu_csv(cpu_csv):
+    cpu_df = pd.read_csv(cpu_csv)
     pass
 
 def read_gpu_csv(gpu_csv):
+    gpu_df = pd.read_csv(gpu_csv)
     pass
 
-def cpu_plot(time, temp, usage):
-    pass
+def stat_plot(time, temp, usage):
+    """
+    function that plots the stats of selected hardware
+    ----------------------------------------------------------------------------
+    time: ndarray
 
-def gpu_plot(time, temp, usage):
-    pass
+    time of 
+    """
+    fig, ax1 = plt.subplots()
+    ax1.scatter(time, temp, label="temperature")
+    ax1.set_xlabel("time [S]")
+    ax1.set_ylabel("temperature [C]")
+
+    ax2 = ax1.twinx()
+    ax2.scatter(time, usage, label="usage")
+    ax2.set_ylabel("usage [%]")
+
+    ax1.legend()
+    ax2.legend()
+
+    pass    
 
 if __name__ == "__main__":
+    cpu_csv = sys.argv[1]
+    gpu_csv = sys.argv[2]
+
     pass
