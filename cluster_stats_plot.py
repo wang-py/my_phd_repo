a11e889c7f7b9a8940d910d50b620df0d1a8348e
MillerMC96 <pywang@ucdavis.edu>
2021-10-20 18:29:53 -0700

    just record temperature

--- cluster_stats_plot.py
+++ cluster_stats_plot.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import sys
 
@@ -7,9 +8,13 @@ import sys
 
 def read_gpu_csv(gpu_csv):
     gpu_df = pd.read_csv(gpu_csv)
-    pass
+    gpu_temp = gpu_df.loc[:, " temperature.gpu"].to_numpy()
+    gpu_usage = gpu_df.loc[:, " utilization.gpu [%]"].str[:-1].astype(int).to_numpy()
 
-def stat_plot(time, temp, usage):
+    
+    return gpu_temp, gpu_usage
+
+def stat_plot(time, temp):
     """
     function that plots the stats of selected hardware
     ----------------------------------------------------------------------------
@@ -18,20 +23,20 @@ def stat_plot(time, temp, usage):
     time of 
     """
     fig, ax1 = plt.subplots()
-    ax1.scatter(time, temp, label="temperature")
+    ax1.scatter(time, temp, label="temperature", color='b')
     ax1.set_xlabel("time [S]")
     ax1.set_ylabel("temperature [C]")
 
-    ax2 = ax1.twinx()
-    ax2.scatter(time, usage, label="usage")
-    ax2.set_ylabel("usage [%]")
-
     ax1.legend()
-    ax2.legend()
 
     pass    
 
 if __name__ == "__main__":
-    gpu_csv = sys.argv[2]
+    gpu_csv = sys.argv[1]
+    gpu_temp, gpu_usage = read_gpu_csv(gpu_csv)
+    time_arr = np.arange(len(gpu_temp))
+    stat_plot(time_arr, gpu_temp)
+
+    plt.show()
 
     pass
